d15b17dfad162ab3dc67d60184a60fc344120379
Cássio <cassiofialho@hotmail.com>
2023-01-19 22:21:20 -0300

    RelacionamentoTabelas

---
+++ Escola/Migrations/20230120004226_Alunos.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Escola.Migrations
+{
+    public partial class Alunos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
